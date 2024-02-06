330336eda9439577851460719e8e6cbe3dfce880
adambarta <adam.barta404@gmail.com>
2024-02-06 20:12:36 +0100

    Initial Commit

---
+++ Quotly/QuotlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotlyApp.swift
+//  Quotly
+//
+//  Created by Adam Barta on 06.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct QuotlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
